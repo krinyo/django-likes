b8a969e6a459fd9f10f2fe26511cb08e3a06f9d5
Крицкий Егор <krea@sibircentr.ru>
2023-05-22 15:58:06 +0500

    Worked with some filters at admin panel

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Rating, Location
 # Register your models here.
-admin.site.register(Rating)
-admin.site.register(Location)
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['location', 'date_created', 'like', 'dislike']
+    list_filter = ['location']
+
+admin.site.register(Rating, RatingAdmin)
+
+class LocationAdmin(admin.ModelAdmin):
+    readonly_fields = ('location_link',)
+
+admin.site.register(Location, LocationAdmin)
+
--- ratings/models.py
+++ ratings/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 class Location(models.Model):
     id = models.AutoField(primary_key=True)
     location_name = models.CharField('Местоположение', max_length=50)
+    location_link = models.CharField('Ссылка на страницу', max_length=200, blank=True, null=True, editable=False)
     location_header = models.CharField('Заголовок', max_length=100, null=True)
     main_image = models.ImageField(upload_to='location_images/', null=True)
     like_image = models.ImageField(upload_to='location_images/', blank=True, null=True)
     dislike_image = models.ImageField(upload_to='location_images/', blank=True, null=True)
+
     def __str__(self):
         return self.location_name
-
+    def save(self, *args, **kwargs):
+        if not self.location_link:
+            self.location_link = reverse('ratings:location_rating', args=[self.location_name])
+        super().save(*args, **kwargs)
     class Meta:
+        verbose_name = "Местоположение"
         verbose_name_plural = "Местоположения"
 
 class Rating(models.Model):
@@ -23,7 +30,10 @@ class Rating(models.Model):
 
     def __str__(self):
         date_formatted = self.date_created.strftime("%d.%m.%Y %H:%M")
-        return f"Оценка для {self.location.location_name} ({date_formatted})"
+        result_formatted = "Лайк" if self.like else "Дизлайк"
+        return f"{result_formatted} {date_formatted}"
+        #return f"Оценка {self.location.location_name} ({date_formatted})"
 
     class Meta:
+        verbose_name = "Оценка"
         verbose_name_plural = "Оценки"
